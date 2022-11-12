dc40e6ab87299878d73d3fe1e899803b98b70ce7
RobertSandiford <mail@robertsandiford.co.uk>
2022-11-12 23:41:02 +0000

    save

--- src/defaultProps.js
+++ src/defaultProps.js
@@ -1,6 +1,6 @@
 // @flow
 
-import Prism from "./vendor/prism.js";
+import Prism from "./vendor/prism/index.js";
 import theme from "./themes/duotoneDark.js";
 
 import type { PrismLib } from "./types";
